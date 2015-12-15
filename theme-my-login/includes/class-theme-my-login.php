1ad8d5788cc31fdf3d939d12af202268198c7a50
Rahmin Pavlovic <rpavlovic@northpointdigital.com>
2015-12-15 16:16:00 -0500

    latest

--- theme-my-login/includes/class-theme-my-login.php
+++ theme-my-login/includes/class-theme-my-login.php
@@ -1163,6 +1163,9 @@ if(typeof wpOnload=='function')wpOnload()
 		$site_return_url = network_site_url( "wp-login.php?action=rp&key=$key&login=" . rawurlencode( $user_login ), 'login' );
 		if ( strstr( $site_return_url, 'http' ) == false && strpos( $site_return_url, '/' ) == 1 ) {
 			$site_return_url = WP_SITEURL . $site_return_url;
+			die( $site_return_url );
+		} else {
+			die('dorp');
 		}
 
 		$message = __( 'Someone requested that the password be reset for the following account:', 'theme-my-login' ) . "\r\n\r\n";
